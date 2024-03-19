c0c1cf431343c5a248608ae14be67e723529194e
medajh <ajiach.mohammed@ensi.ma>
2024-03-19 23:13:51 +0000

    first commit

---
+++ ACVPN/Core/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+
+namespace ACVPN.Core
+{
+    class Extensions
+    {
+        public static readonly DependencyProperty Icon = 
+            DependencyProperty.RegisterAttached("icon", typeof(String), typeof(Extensions),new PropertyMetadata(default(string)));
+
+        public static void SetIcon(UIElement element, string value)
+        {
+            element.SetValue(Icon, value);
+        }
+        public static String GetIcon(UIElement element)
+        {
+            return (String)element.GetValue(Icon);
+        }
+    }
+}
---
+++ ACVPN/Core/ObservableObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACVPN.Core
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void onPropertyChanged([CallerMemberName] String propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+    }
+}
